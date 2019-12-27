21526461b9b4701d0072c6cabd68f65d9f1a8cce
Freek de Bruijn <freekdb@gmail.com>
2019-12-27 12:40:08 +0100

    Step 1: Hello Kotlin!

---
+++ src/main/kotlin/com/github/freekdb/kotlinintro/step01/HelloKotlin.kt
@@ -0,0 +1,9 @@
+package com.github.freekdb.kotlinintro.step01
+
+fun main() = println("Hello Kotlin!")
+
+//fun main(arguments: Array<String>) {
+//    arguments.forEach {
+//        println("Hello $it!")
+//    }
+//}
