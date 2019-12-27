b363a0b5f51a494d70e703b930f1037672392f47
Freek de Bruijn <freekdb@gmail.com>
2019-12-27 12:40:51 +0100

    Step 1: Variables

---
+++ src/main/kotlin/com/github/freekdb/kotlinintro/step02/Variables.kt
@@ -0,0 +1,27 @@
+package com.github.freekdb.kotlinintro.step02
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.minutes
+
+@ExperimentalTime
+fun main() {
+    // Immutable variable with type inference (Duration).
+    val workshopDuration = 150.minutes
+    println("Workshop: ${workshopDuration.inHours} hours.")
+
+    // Mutable variable with type inference (Int).
+    var kotlinReasons = 4
+    kotlinReasons += 6
+    println("Kotlin reasons: $kotlinReasons (and counting).")
+
+
+    println()
+
+
+    // Mutable variable with type nullable String.
+    var favoriteMovie: String? = null
+    println("Favorite movie: $favoriteMovie.")
+
+    favoriteMovie = "The Shawshank Redemption"
+    println("New favorite movie: $favoriteMovie.")
+}
