19890c54b200b0158cb097241e013334482a848d
Freek de Bruijn <freekdb@gmail.com>
2020-01-07 01:11:31 +0100

    Step 3: Lists with numbers

--- src/main/kotlin/com/github/freekdb/kotlin/workshop/step03/ListsWithNumbers.kt
+++ src/main/kotlin/com/github/freekdb/kotlin/workshop/step03/ListsWithNumbers.kt
@@ -1,8 +1,11 @@
-package com.github.freekdb.kotlinintro.step03
+package com.github.freekdb.kotlin.workshop.step03
 
 fun main() {
     val numbers = listOf(1, 2, 4, 8, 16, 32)
     val numbersSquared = numbers.map { it * it }
 
     println("Numbers squared: $numbersSquared")
+
+    val perfectNumber = numbers[1] + numbers[2]
+    println("Perfect number: $perfectNumber.")
 }
